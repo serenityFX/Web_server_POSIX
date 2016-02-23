a24eadc5fb701c4cb21b919cc4dd8f4456744855
victoryagudaev@gmail.com <victoryagudaev@gmail.com>
2016-02-23 13:15:15 +0400

    solves errors

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc , char *argv[])
         write(new_socket , message , strlen(message));
          
         pthread_t sniffer_thread;
-        new_sock = malloc(1);
+        new_sock = static_cast<int *>(malloc(1));
         *new_sock = new_socket;
          
         if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
@@ -80,16 +80,33 @@ void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
-     
-    char *message;
+    int read_size;
+    char *message , client_message[2000];
      
     //Send some messages to the client
     message = "Greetings! I am your connection handler\n";
     write(sock , message , strlen(message));
      
-    message = "Its my duty to communicate with you";
+    message = "Now type something and i shall repeat what you type \n";
     write(sock , message , strlen(message));
      
+    //Receive a message from client
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        write(sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
     //Free the socket pointer
     free(socket_desc);
      
